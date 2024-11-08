a37da4aa3e54ddcd0bb40bc5fb11aea6f08ea20a
Michael Mumme <neptica@hotmail.com>
2024-11-08 13:01:58 -0600

    add new subproject creation

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,3 +1,6 @@
+import check from "./svg/check.svg";
+import plus from "./svg/plus.svg";
+
 export default (function (projects) {
   function init() {
     renderDashboard();
@@ -5,21 +8,26 @@ export default (function (projects) {
 
   function renderDashboard() {
     const dash = document.getElementById("dashboard");
-    let eachProject = [];
-
-    for (const [project, indProject] of Object.entries(projects)) {
-      eachProject.push([project, 0]);
-    }
+    dash.innerHTML = "";
 
     for (const [project, indProject] of Object.entries(projects)) {
       const projDiv = document.createElement("div");
+
       projDiv.classList.add("proj");
+      const titleCard = document.createElement("div");
       const projTitle = document.createElement("h1");
+      const addMore = document.createElement("img");
+
+      addMore.src = plus;
+      addMore.addEventListener("click", insertInput);
       projTitle.textContent = project;
       projTitle.dataset.elementType = "h1";
-      eachProject[project] = false;
       projTitle.addEventListener("dblclick", changeToInput);
-      projDiv.appendChild(projTitle);
+      titleCard.classList.add("titleCard");
+
+      titleCard.appendChild(projTitle);
+      titleCard.appendChild(addMore);
+      projDiv.appendChild(titleCard);
 
       for (const [iProject, values] of Object.entries(indProject)) {
         const iProjDiv = document.createElement("p");
@@ -32,6 +40,21 @@ export default (function (projects) {
     }
   }
 
+  function insertInput() {
+    const projInput = document.createElement("input");
+    projInput.classList.add("indProj");
+    projInput.dataset.elementType = "p";
+
+    projInput.addEventListener("focusout", convertBack);
+    projInput.addEventListener("keypress", function (event) {
+      if (event.key == "Enter") convertBack.call(projInput);
+    });
+
+    this.parentNode.after(projInput);
+
+    projInput.focus(); // this makes it focus nicely
+  }
+
   function changeToInput() {
     let currentTitle = this.textContent;
 
@@ -41,6 +64,7 @@ export default (function (projects) {
     projInput.style.fontWeight = getComputedStyle(this).fontWeight;
     projInput.value = currentTitle;
     projInput.dataset.elementType = this.dataset.elementType;
+    projInput.dataset.oldText = currentTitle;
     if (projInput.dataset.elementType == "p")
       projInput.classList.add("indProj");
     projInput.addEventListener("focusout", convertBack);
@@ -48,10 +72,22 @@ export default (function (projects) {
       if (event.key == "Enter") convertBack.call(projInput);
     });
     this.parentNode.replaceChild(projInput, this);
+
+    projInput.focus(); // this makes it focus nicely
   }
 
   function convertBack() {
     let currentTitle = this.value;
+    if (currentTitle == "") {
+      if (typeof this.dataset.oldText === "undefined") {
+        this.parentNode.removeChild(this);
+        return;
+      }
+
+      console.log(this.dataset.oldText);
+      currentTitle = this.dataset.oldText;
+    }
+
     const currentType = this.dataset.elementType;
 
     const projTitle = document.createElement(currentType);
